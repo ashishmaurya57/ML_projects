1dbe10776318f4b0252b3840d3d3fc034642ce3d
ashishmaurya57 <ashishmauryaam57@gmail.com>
2025-01-28 02:52:12 +0530

    final

--- src/utils.py
+++ src/utils.py
@@ -1,47 +1,67 @@
 import os
-import sys 
-import numpy as np
+import sys
+
+import numpy as np 
 import pandas as pd
 import dill
+import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
-from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
-        dir_path=os.path.dirname(file_path)
+        dir_path = os.path.dirname(file_path)
 
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path,"wb") as file_obj:
-            dill.dump(obj, file_obj)
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
     except Exception as e:
-        raise CustomException(e, sys)        
-
-def evaluate_model(x_train, y_train, x_test, y_test, models, params):
-    try:
-        report={}
-        for i in range(len(list(models))):
-            model=list(models.values())[i]
-            para=params[list(models.keys())[i]]
-            grid=GridSearchCV(model, param_grid=para, cv=3)
-            grid.fit(x_train,y_train)
-            model.set_params(**grid.best_params_)
-            model.fit(x_train, y_train)
-
-
-
-            # model.fit(x_train, y_train)
-            y_train_pred=model.predict(x_train)
-            y_test_pred=model.predict(x_test)
-            train_model_Score=r2_score(y_train, y_train_pred)
-            test_model_score=r2_score(y_test, y_test_pred)
-            report[list(models.keys())[i]]=test_model_score
-        return report
-    except Exception as e:
-        raise CustomException(e, sys) 
-
-
+        raise CustomException(e, sys)
     
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+           
+            print("Model type:", type(model))
+            try:
+              gs = GridSearchCV(model, param_grid=para, cv=3)
+              gs.fit(X_train, y_train)
+            except Exception as e:
+              print(f"Error during GridSearchCV fitting: {e}")
+              raise
+            model.set_params(**gs.best_params_)
+
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
